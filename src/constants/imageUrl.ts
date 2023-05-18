c1ae8232ee3a9297002adafb16c7484fbcf975d9
hhyuw <high2092@gmail.com>
2023-05-18 23:21:20 +0900

    feat: MOZ 추가

--- src/constants/imageUrl.ts
+++ src/constants/imageUrl.ts
@@ -1,2 +1,3 @@
 export const ELIXIR_ICON_IMAGE_URL = '/image/elixir-icon.png';
 export const MUSIC_NODE_ICON_IMAGE_URL = '/image/music-node-icon.png';
+export const MOZ_ICON_IMAGE_URL = '/image/moz-icon.png';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import { IconSection } from '../components/Icon';
 import { Prompt } from '../components/Prompt';
-import { ELIXIR_ICON_IMAGE_URL, MUSIC_NODE_ICON_IMAGE_URL } from '../constants/imageUrl';
+import { ELIXIR_ICON_IMAGE_URL, MOZ_ICON_IMAGE_URL, MUSIC_NODE_ICON_IMAGE_URL } from '../constants/imageUrl';
 import * as S from '../styles/index.style';
 
 const icons: IconProps[] = [
   { imageUrl: ELIXIR_ICON_IMAGE_URL, text: 'Lostark Elixir Simulator', url: 'https://lostark-elixir.vercel.app' },
   { imageUrl: MUSIC_NODE_ICON_IMAGE_URL, text: 'Music Node Playlist Adapter', url: 'https://music-node-playlist-adapter.vercel.app/' },
+  { imageUrl: MOZ_ICON_IMAGE_URL, text: '모두의 퀴즈', url: 'https://moz-demo.vercel.app/' },
 ];
 
 function Home() {
