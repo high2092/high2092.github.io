88bd6cc9c113f3791fcfdc5f9701268bd9fa0a51
hhyuw <high2092@gmail.com>
2023-10-10 21:54:01 +0900

    feat: 앱 2종 추가

--- src/constants/imageUrl.ts
+++ src/constants/imageUrl.ts
@@ -1,3 +1,5 @@
 export const ELIXIR_ICON_IMAGE_URL = '/image/elixir-icon.png';
+export const TRANSCENDENCE_ICON_IMAGE_URL = '/image/transcendence-icon.png';
+export const MAPLE_SCOUTER_ICON_IMAGE_URL = '/image/maple-scouter-icon.png';
 export const MUSIC_NODE_ICON_IMAGE_URL = '/image/music-node-icon.png';
 export const MOZ_ICON_IMAGE_URL = '/image/moz-icon.png';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import { IconSection } from '../components/Icon';
 import { Prompt } from '../components/Prompt';
-import { ELIXIR_ICON_IMAGE_URL, MOZ_ICON_IMAGE_URL, MUSIC_NODE_ICON_IMAGE_URL } from '../constants/imageUrl';
+import { ELIXIR_ICON_IMAGE_URL, MAPLE_SCOUTER_ICON_IMAGE_URL, MOZ_ICON_IMAGE_URL, MUSIC_NODE_ICON_IMAGE_URL, TRANSCENDENCE_ICON_IMAGE_URL } from '../constants/imageUrl';
 import * as S from '../styles/index.style';
 
 const icons: IconProps[] = [
-  { imageUrl: ELIXIR_ICON_IMAGE_URL, text: 'Lostark Elixir Simulator', url: 'https://lostark-elixir.vercel.app' },
-  { imageUrl: MUSIC_NODE_ICON_IMAGE_URL, text: 'Music Node Playlist Adapter', url: 'https://music-node-playlist-adapter.vercel.app/' },
+  { imageUrl: ELIXIR_ICON_IMAGE_URL, text: 'LostArk Elixir Simulator', url: 'https://lostark-elixir.vercel.app' },
+  { imageUrl: TRANSCENDENCE_ICON_IMAGE_URL, text: 'LostArk Transcendence Simulator', url: 'https://lostark-transcendence.vercel.app' },
+  { imageUrl: MAPLE_SCOUTER_ICON_IMAGE_URL, text: 'Maple Scouter', url: 'https://maplescouter.vercel.app' },
+  { imageUrl: MUSIC_NODE_ICON_IMAGE_URL, text: 'Music Node', url: 'https://mnode.vercel.app/' },
   { imageUrl: MOZ_ICON_IMAGE_URL, text: '모두의 퀴즈', url: 'https://moz-demo.vercel.app/' },
 ];
 
